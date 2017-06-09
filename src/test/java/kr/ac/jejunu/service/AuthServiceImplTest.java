24f8c5605834371e10bb9550fd776a97227fa086
Dongha Kim <ghost9087@kailua.co.kr>
2017-06-10 01:57:35 +0900

    AuthServiceTest 추가

---
+++ src/test/java/kr/ac/jejunu/service/AuthServiceImplTest.java
@@ -0,0 +1,105 @@
+package kr.ac.jejunu.service;
+
+import kr.ac.jejunu.model.Restaurant;
+import kr.ac.jejunu.model.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.*;
+
+/**
+ * Created by ghost9087 on 10/06/2017.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class AuthServiceImplTest {
+    @Configuration
+    static class AuthServiceTestContextConfiguration {
+        @Bean
+        public AuthService authService() {
+            return new AuthServiceImpl();
+        }
+
+        @Bean
+        public UserDetailsService userDetailsService(){
+            return mock(UserDetailsService.class);
+        }
+
+        @Bean
+        public AuthenticationManager authenticationManager() {
+            return mock(AuthenticationManager.class);
+        }
+
+        @Bean
+        public UserService userService() {
+            return mock(UserService.class);
+        }
+    }
+
+    @Autowired
+    private AuthService sut;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private User user;
+
+    @Before
+    public void setUp() throws Exception {
+        user = new User();
+        user.setId(1);
+        user.setName("test_user");
+        user.setPassword("1234");
+        reset(userDetailsService, authenticationManager);
+    }
+
+    @Test
+    public void testValidLogin() throws Exception {
+        UserDetails mockUserDetails = mock(UserDetails.class);
+        when(userDetailsService.loadUserByUsername(anyString())).thenReturn(mockUserDetails);
+        when(authenticationManager.authenticate(ArgumentMatchers.any(Authentication.class))).then(new Answer<Object>() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                Authentication authentication = (Authentication) invocation.getArguments()[0];
+                Authentication spy = spy(authentication);
+                when(spy.isAuthenticated()).thenReturn(true);
+                return null;
+            }
+        });
+
+        sut.authByUser(user);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assertThat(authentication, notNullValue());
+    }
+
+    @Test
+    public void testLogout() throws Exception {
+        sut.deauthorize();
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        assertThat(authentication, nullValue());
+    }
+}
--- src/test/java/kr/ac/jejunu/service/CommentServiceImplTest.java
+++ src/test/java/kr/ac/jejunu/service/CommentServiceImplTest.java
@@ -9,7 +9,6 @@ import kr.ac.jejunu.repository.RestaurantRepository;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
