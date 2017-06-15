7a952dc581a8b70aa5322e8953bf919d1080dec2
Dongha Kim <ghost9087@kailua.co.kr>
2017-06-16 02:31:15 +0900

    signup추가

--- src/main/java/kr/ac/jejunu/controller/web/UserWebController.java
+++ src/main/java/kr/ac/jejunu/controller/web/UserWebController.java
@@ -1,9 +1,13 @@
 package kr.ac.jejunu.controller.web;
 
+import kr.ac.jejunu.exceptions.ObjectDuplicatedException;
+import kr.ac.jejunu.model.User;
+import kr.ac.jejunu.service.AuthService;
+import kr.ac.jejunu.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by ghost9087 on 12/06/2017.
@@ -11,6 +15,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/auth")
 public class UserWebController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AuthService authService;
+
     @RequestMapping("/login")
     public String login() {
         return "login";
@@ -24,6 +34,29 @@ public class UserWebController {
 
     @GetMapping("/sign-up")
     public String signUp() {
+        return "signup";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(@ModelAttribute User user) {
+        User savedUser = userService.addUser(user);
+
+        authService.authByUser(savedUser);
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public String illegalSignUp(Model model, IllegalArgumentException ex) {
+        model.addAttribute("error", true);
+        model.addAttribute("message", ex.getMessage());
+        return "signup";
+    }
+
+    @ExceptionHandler(ObjectDuplicatedException.class)
+    public String illegalSignUp(Model model, ObjectDuplicatedException ex) {
+        model.addAttribute("error", true);
+        model.addAttribute("message", ex.getMessage());
         return "signup";
     }
 }
